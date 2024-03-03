bf586c209e7ae0c08a91e05f0d2e857549425c4c
Adam Buran <aburan28@gmail.com>
2024-03-02 17:44:29 -0800

    add more code

--- pkg/aws.go
+++ pkg/aws.go
@@ -1,28 +1,28 @@
 package pkg
 
-
 import (
-"github.com/aws/aws-sdk-go-v2/aws"
-"github.com/aws/aws-sdk-go-v2/config"
-"github.com/aws/aws-sdk-go-v2/service/eks"
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/eks"
 )
 
-
 type NodegroupUpdater struct {
-	Regions  []string
-	Clusters map[string]string
-	Nodegroups []string
-	Config map[string]aws.Config
+	Regions    []string
+	Clusters   map[string][]string
+	Nodegroups map[string][]string
+	Configs    map[string]aws.Config
 }
 
 func NewNodegroupUpdater(regions []string, clusters []string, nodegroups []string) *NodegroupUpdater {
 	updater := &NodegroupUpdater{
 		Regions: regions,
-		Clusters: clusters,
-		Nodegroups: nodegroups,
-		Config: make(map[string]aws.Config),
+		Configs: make(map[string]aws.Config),
 	}
 	updater.initConfigs()
+	return updater
 }
 
 func (n *NodegroupUpdater) initConfigs() {
@@ -38,83 +38,48 @@ func (n *NodegroupUpdater) initConfigs() {
 }
 
 func (n *NodegroupUpdater) DiscoverClusters() error {
+	// find all clusters in the regions configured and add those clusters to a map indexed by region
 	for _, region := range n.Regions {
 		cfg := n.Configs[region]
 		client := eks.NewFromConfig(cfg)
 		input := &eks.ListClustersInput{}
-		req := client.ListClustersRequest(input)
-		resp, err := req.Send(context.TODO())
+		resp, err := client.ListClusters(context.TODO(), input)
 		if err != nil {
 			return err
 		}
 		for _, cluster := range resp.Clusters {
-			n.Clusters[cluster] = region
+			n.Clusters[region] = append(n.Clusters[region], cluster)
 		}
+
 	}
+	return nil
 }
 
-func (n *NodegroupUpdater) DiscoverNodegroups(cluster string, region string) error {
-	cfg := n.Configs[region]
-	client := eks.NewFromConfig(cfg)
-	input := &eks.ListNodegroupsInput{
-		ClusterName: aws.String(cluster),
-	}
-	req := client.ListNodegroupsRequest(input)
-	resp, err := req.Send(context.TODO())
-	if err != nil {
-		return err
-	}
-	for _, nodegroup := range resp.Nodegroups {
-		n.Nodegroups = append(n.Nodegroups, nodegroup)
-	}
+func (n *NodegroupUpdater) DiscoverNodegroups() error {
 
-}
-
-func (n *NodegroupUpdater) ListUpdatesPending(cluster string, region string) error {
-	cfg := n.Configs[region]
-	client := eks.NewFromConfig(cfg)
-	input := &eks.DescribeNodegroupInput{
-		ClusterName: aws.String(cluster),
-		NodegroupName: aws.String(nodegroup),
-	}
-	req := client.DescribeNodegroupRequest(input)
-	resp, err := req.Send(context.TODO())
-	if err != nil {
-		return err
-	}
-	// Check if there are updates pending
-
-	
-}
-
-
-
-
-func (n *NodegroupUpdater) UpdateNodegroup(nodegroup string, region string) error {
 	for _, region := range n.Regions {
-		for _, cluster := range n.Clusters {
-			for _, nodegroup := range n.Nodegroups {
-				// Update the nodegroup
+		for _, cluster := range n.Clusters[region] {
+
+			cfg := n.Configs[region]
+			client := eks.NewFromConfig(cfg)
+			input := &eks.ListNodegroupsInput{
+				ClusterName: aws.String(cluster),
+			}
+			resp, err := client.ListNodegroups(context.TODO(), input)
+
+			if err != nil {
+				return err
+			}
+
+			for _, nodegroup := range resp.Nodegroups {
+				n.Nodegroups[region] = append(n.Nodegroups[region], nodegroup)
 			}
 		}
 	}
+	return nil
 }
 
-func main(){
-	for _, region := range regions {
-		for _, cluster := range clusters {
-			for _, nodegroup := range nodegroups {
-				// Update the nodegroup
-			}
-		}
-	}
+func (n *NodegroupUpdater) UpgradeNodegroup(cluster string, region string, force bool) error {
 
-
-
-
-	cfg, err := config.LoadDefaultConfig(context.TODO(),
-		config.WithRegion("us-west-2"),
-
-	client := eks.NewFromConfig(cfg)
-
+	return nil
 }
